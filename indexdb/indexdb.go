451bfb9739a69f9507d5fcfbb5e7aba0ebfe5db9
sgsdxzy <sgsdxzy@gmail.com>
2013-08-10 20:22:46 +0800

    Implemented the preliminary DB

--- indexdb/indexdb.go
+++ indexdb/indexdb.go
@@ -3,6 +3,7 @@ package indexdb
 import (
 	"encoding/binary"
 	"io"
+	"os"
 )
 
 //Serializer warpper of basic types
@@ -15,31 +16,24 @@ type Float64Serializer float64
 //Serialize writes serialized bytes to io.Writer and returns number of bytes wrote
 //Deserialize reads bytes from io.Reader
 type Serializer interface {
-	Serialize(w io.Writer) int64
+	Serialize(w io.Writer)
 	Deserialize(r io.Reader)
 }
 
 //Inputer for Index DataBase
 type inputer struct {
-	offset int64
 	writer io.WriteSeeker
 }
 
 func NewInputer(w io.WriteSeeker) *inputer {
-	return &inputer{0, w}
+	return &inputer{w}
 }
 
 //Put s into database, numeric item id is returned
 func (i *inputer) Input(s Serializer) int64 {
-	start := i.offset
-	i.writer.Seek(start, 0)
-	i.offset += s.Serialize(i.writer)
+        start, _ := i.writer.Seek(0, 2)
+	s.Serialize(i.writer)
 	return start
-}
-
-//Get current offset
-func (i *inputer) GetOffset() int64 {
-	return i.offset
 }
 
 //Outputer for Index DataBase
@@ -52,9 +46,44 @@ func NewOutputer(r io.ReadSeeker) *outputer {
 }
 
 //Reads data at offset to s
-func (o *outputer) Output(s Serializer, offset int64) {
+func (o *outputer) Output(offset int64, s Serializer) {
 	o.reader.Seek(offset, 0)
 	s.Deserialize(o.reader)
+}
+
+//The full database
+type indexdb struct {
+	file *os.File
+	i    inputer
+	o    outputer
+}
+
+func NewIndexDB(path string) *indexdb {
+	db := new(indexdb)
+	db.file ,_ = os.OpenFile(path, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	db.i = inputer{db.file}
+	db.o = outputer{db.file}
+        return db
+}
+
+func (db *indexdb) AddEntry(key Serializer, value Serializer) int64 {
+	id := db.i.Input(value)
+	db.i.Input(key)
+	return id
+}
+
+func (db *indexdb) GetValue(id int64, value Serializer) {
+	db.o.Output(id, value)
+}
+
+func (db *indexdb) GetKeyAndValue(id int64, key, value Serializer) {
+	db.o.reader.Seek(id, 0)
+	value.Deserialize(db.o.reader)
+	key.Deserialize(db.o.reader)
+}
+
+func (db *indexdb) Close() {
+	db.file.Close()
 }
 
 //Implementations of Serializer wrappers
@@ -68,9 +97,8 @@ func (i *Int32Serializer) Get() int32 {
 	return int32(*i)
 }
 
-func (i *Int32Serializer) Serialize(w io.Writer) int64 {
+func (i *Int32Serializer) Serialize(w io.Writer) {
 	binary.Write(w, binary.LittleEndian, i)
-	return int64(binary.Size(i))
 }
 
 func (i *Int32Serializer) Deserialize(r io.Reader) {
@@ -87,9 +115,8 @@ func (i *Int64Serializer) Get() int64 {
 	return int64(*i)
 }
 
-func (i *Int64Serializer) Serialize(w io.Writer) int64 {
+func (i *Int64Serializer) Serialize(w io.Writer) {
 	binary.Write(w, binary.LittleEndian, i)
-	return int64(binary.Size(i))
 }
 
 func (i *Int64Serializer) Deserialize(r io.Reader) {
@@ -106,9 +133,8 @@ func (i *Float32Serializer) Get() float32 {
 	return float32(*i)
 }
 
-func (i *Float32Serializer) Serialize(w io.Writer) int64 {
+func (i *Float32Serializer) Serialize(w io.Writer) {
 	binary.Write(w, binary.LittleEndian, i)
-	return int64(binary.Size(i))
 }
 
 func (i *Float32Serializer) Deserialize(r io.Reader) {
@@ -125,9 +151,8 @@ func (i *Float64Serializer) Get() float64 {
 	return float64(*i)
 }
 
-func (i *Float64Serializer) Serialize(w io.Writer) int64 {
+func (i *Float64Serializer) Serialize(w io.Writer) {
 	binary.Write(w, binary.LittleEndian, i)
-	return int64(binary.Size(i))
 }
 
 func (i *Float64Serializer) Deserialize(r io.Reader) {

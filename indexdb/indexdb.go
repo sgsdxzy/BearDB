d77d32150d87564dd11d4814bb6a82138769daa9
sgsdxzy <sgsdxzy@gmail.com>
2013-08-10 21:50:33 +0800

    Changed DB to compactDB

--- indexdb/indexdb.go
+++ indexdb/indexdb.go
@@ -8,6 +8,7 @@ import (
 
 //Serializer warpper of basic types
 
+type NullSerializer struct{} //Place holder that does nothing
 type Int32Serializer int32
 type Int64Serializer int64
 type Float32Serializer float32
@@ -15,6 +16,8 @@ type Float64Serializer float64
 
 //Serialize writes serialized bytes to io.Writer and returns number of bytes wrote
 //Deserialize reads bytes from io.Reader
+//Serializers may always be pointers as Deserialize need to change the
+//contents of it
 type Serializer interface {
 	Serialize(w io.Writer)
 	Deserialize(r io.Reader)
@@ -31,7 +34,7 @@ func NewInputer(w io.WriteSeeker) *inputer {
 
 //Put s into database, numeric item id is returned
 func (i *inputer) Input(s Serializer) int64 {
-        start, _ := i.writer.Seek(0, 2)
+	start, _ := i.writer.Seek(0, os.SEEK_END)
 	s.Serialize(i.writer)
 	return start
 }
@@ -46,47 +49,67 @@ func NewOutputer(r io.ReadSeeker) *outputer {
 }
 
 //Reads data at offset to s
-func (o *outputer) Output(offset int64, s Serializer) {
-	o.reader.Seek(offset, 0)
+func (o *outputer) Output(offset int64, s Serializer) Serializer {
+	o.reader.Seek(offset, os.SEEK_SET)
 	s.Deserialize(o.reader)
+	return s
 }
 
-//The full database
+//The compact database
 type indexdb struct {
-	file *os.File
-	i    inputer
-	o    outputer
+	file  *os.File
+	embed bool //Whether embed keys in db
+	i     inputer
+	o     outputer
 }
 
-func NewIndexDB(path string) *indexdb {
+func NewIndexDB(path string, e bool) *indexdb {
 	db := new(indexdb)
-	db.file ,_ = os.OpenFile(path, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	db.file, _ = os.OpenFile(path, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	db.embed = e
 	db.i = inputer{db.file}
 	db.o = outputer{db.file}
-        return db
+	return db
+}
+
+func (db *indexdb) Embed() bool {
+        return db.embed
 }
 
 func (db *indexdb) AddEntry(key Serializer, value Serializer) int64 {
 	id := db.i.Input(value)
-	db.i.Input(key)
+	if db.embed {
+		db.i.Input(key)
+	}
 	return id
 }
 
-func (db *indexdb) GetValue(id int64, value Serializer) {
-	db.o.Output(id, value)
+func (db *indexdb) GetValue(id int64, value Serializer) Serializer {
+	return db.o.Output(id, value)
 }
 
-func (db *indexdb) GetKeyAndValue(id int64, key, value Serializer) {
-	db.o.reader.Seek(id, 0)
+//If embed==false, key is not changed and directly returned
+func (db *indexdb) GetKeyAndValue(id int64, key, value Serializer) (Serializer, Serializer) {
+	db.o.reader.Seek(id, os.SEEK_SET)
 	value.Deserialize(db.o.reader)
-	key.Deserialize(db.o.reader)
+	if db.embed {
+		key.Deserialize(db.o.reader)
+	}
+	return key, value
 }
 
+//Make sure to close it before exit!
 func (db *indexdb) Close() {
 	db.file.Close()
 }
 
 //Implementations of Serializer wrappers
+//=============================================================================
+
+//Do nothing
+func (i *NullSerializer) Serialize(w io.Writer)   {}
+func (i *NullSerializer) Deserialize(r io.Reader) {}
+
 //=============================================================================
 
 func (i *Int32Serializer) Set(n int32) {
